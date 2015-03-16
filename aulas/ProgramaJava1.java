25a13fa6131cf5f4da7bbfd5652247f61476509b
Paulorjr <Paulorjr98@gmail.com>
2015-03-16 10:14:07 -0300

    Create ProgramaJava1.java
    Primeiro programa em Java ! Aula1

---
+++ aulas/ProgramaJava1.java
@@ -0,0 +1,7 @@
+public class ProgramaJava1{
+	public static void main(string[]args){
+	
+		/* Imprime no console a frase: 'Meu primeiro Programa em java' */
+		System.out.println("Meu primeiro programa em java");
+	}
+}
