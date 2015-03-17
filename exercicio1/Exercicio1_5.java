1a18843658c03efb43a7564cd724ce5b32bded10
Paulorjr <Paulorjr98@gmail.com>
2015-03-17 11:22:20 -0300

    Create Exercicio1_5.java
    Resposta exercício 5

---
+++ exercicio1/Exercicio1_5.java
@@ -0,0 +1,19 @@
+public class Exercicio1_5{
+	public static void main(String[]args){
+		double a, b, c, soma, mult, sub,div;
+
+		a = 45;
+		b = 20;
+		c = 3;
+
+		soma = a + b + c;
+		mult = a * b * c;
+		div = a / b / c;
+		sub  = a - b - c;
+
+		System.out.println("O valor da soma foi: " +soma);
+		System.out.println("O valor da multiplicação foi: " + mult);
+		System.out.println("O valor da divisão foi: " + div);
+		System.out.println("O valor da subtração foi: " + sub);
+	}
+}
